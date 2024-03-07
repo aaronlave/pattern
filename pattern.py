8b2fa1c5907f549e9ab7772d6f3445674f39ca0b
aaronlave <aaronmlavery@gmail.com>
2024-03-07 15:30:01 +0000

    Added user input functionality

--- pattern.py
+++ pattern.py
@@ -1,7 +1,7 @@
-rows = 9
+rows = int(input("How long would you like the pattern to be? "))
 
 for i in range(1, rows +1):
 
-    number_of_stars = i if i <= 5 else rows - i +1
+    number_of_stars = i if i <= (rows / 2) else rows - i +1
 
     print("*" * number_of_stars)
